4a507a867e96a143ba9c83cd4469bde9e24fedc1
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-10-28 11:27:34 -0500

    firebase hooked in

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import './App.css';
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, Redirect } from "react-router-dom";
 import ConfluenceDash from './components/ConfluenceDash'
+import { Component } from 'react';
+
+class App extends Component {
+
+  makeId = (length)=> {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result
+  }
+  
+  handleNewConfluence = () => {
+    let confluenceId = this.makeId(10)
+
+    
+    
+  }
 
 
 
-
-function App() {
-  return (
-    <Switch>
-      <Route exact path='/'>
-        <div className="App">
-          <h2>confluence.</h2>
-
-          <Link to='/confluence'>
-            <button>create a new confluence</button>
-          </Link>
-        </div>
-      </Route>
-
-      <Route path='/confluence'>
-        <ConfluenceDash/>
-      </Route>
-    </Switch>
-  );
+  render(){
+    return (
+      <Switch>
+        <Route exact path='/'>
+          <div className="App">
+            <h2>confluence.</h2>
+  
+            {/* <Link to='/confluence'>
+              <button>create a new confluence</button>
+            </Link> */}
+             <button onClick={this.handleNewConfluence}>create a new confluence</button>
+          </div>
+        </Route>
+  
+        <Route path='/confluence'>
+          <ConfluenceDash/>
+        </Route>
+      </Switch>
+    );
+  }
 }
 
 export default App;
